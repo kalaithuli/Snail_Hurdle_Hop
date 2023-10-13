453ddc6d9576e6c13e80917d633b632405c6ca99
Shiva4113 <shivagolugula123@gmail.com>
2023-10-13 23:20:44 +0530

    Added Play pause button P

--- runner_class.py
+++ runner_class.py
@@ -85,13 +85,15 @@ def collision_sprite():
 		return False
 	else: return True
 
-
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+
+# Create the game window
+screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = False
+game_paused = False
 start_time = 0
 score = 0
 
@@ -104,57 +106,64 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
-player_stand = pygame.transform.rotozoom(player_stand,0,2)
-player_stand_rect = player_stand.get_rect(center = (400,200))
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center=(400, 200))
 
-game_name = test_font.render('Snail Hurdle Hop',False,(111,196,169))
-game_name_rect = game_name.get_rect(center = (400,80))
+game_name = test_font.render('Snail Hurdle Hop', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center=(400, 80))
 
-game_message = test_font.render('Press space to run',False,(111,196,169))
-game_message_rect = game_message.get_rect(center = (400,330))
+game_message = test_font.render('Press space to run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center=(400, 330))
 
 obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer,1500)
+pygame.time.set_timer(obstacle_timer, 1500)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
-		if game_active:
-			if event.type == obstacle_timer:
-				obstacle_group.add(Obstacle(choice(['fly','snail','snail','snail'])))
-		
-		else:
-			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-				game_active = True
-				start_time = int(pygame.time.get_ticks() / 1000)
+        if game_active:
+            if event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(['fly', 'snail', 'snail', 'snail'])))
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                game_paused = not game_paused
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                start_time = int(pygame.time.get_ticks() / 1000)
 
+    if game_active and not game_paused:
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
+        score = display_score()
 
-	if game_active:
-		screen.blit(sky_surface,(0,0))
-		screen.blit(ground_surface,(0,300))
-		score = display_score()
-		
-		player.draw(screen)
-		player.update()
+        player.draw(screen)
+        player.update()
 
-		obstacle_group.draw(screen)
-		obstacle_group.update()
+        obstacle_group.draw(screen)
+        obstacle_group.update()
 
-		game_active = collision_sprite()
-		
-	else:
-		screen.fill((94,129,162))
-		screen.blit(player_stand,player_stand_rect)
+        game_active = collision_sprite()
 
-		score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
-		score_message_rect = score_message.get_rect(center = (400,330))
-		screen.blit(game_name,game_name_rect)
+    else:
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
 
-		if score == 0: screen.blit(game_message,game_message_rect)
-		else: screen.blit(score_message,score_message_rect)
+        score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center=(400, 330))
+        screen.blit(game_name, game_name_rect)
 
-	pygame.display.update()
-	clock.tick(60)
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
+
+        if game_paused:
+            pause_message = test_font.render('Paused', False, (255, 0, 0))  # Red "Paused" message
+            pause_message_rect = pause_message.get_rect(center=(400, 200))
+            screen.blit(pause_message, pause_message_rect)
+
+    pygame.display.update()
+    clock.tick(60)
